1dbc9f56769a543837fffc6698c90fe9fde815d4
miguemesen <miguemesen12@gmail.com>
2021-08-13 02:59:40 -0600

    Add redis server, working login and cookies

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,21 @@ import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
+import {
+    ApolloServerPluginLandingPageGraphQLPlayground
+  } from "apollo-server-core";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+//import { MyContext } from "./types";
+
+
+
+
 
 const main = async () => { // create async main bcs of promises
     const orm = await MikroORM.init(microConfig); 
@@ -15,12 +26,41 @@ const main = async () => { // create async main bcs of promises
 
     const app = express();
 
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient()
+
+    redisClient.on('error',function(err){
+        console.log('Reddis error: ', err)
+    });
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({ 
+                client: redisClient,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+                httpOnly: true,
+                sameSite: 'lax', // csrf
+                secure: __prod__ // cookie only works in https 
+            },
+            saveUninitialized: false,
+            secret: 'keyboard cat',
+            resave: false,
+        })
+    )
+
     const apolloServer = new ApolloServer({
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground(),
+          ],
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver,UserResolver],
             validate: false,
         }),
-        context: () => ({em: orm.em}) // Special object that is accessible to all the resolvers
+        context: ({req,res})=> ({em: orm.em, req, res}) // Special objects that are accessible to all the resolvers
     });
 
     await apolloServer.start();
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import {Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType } from "type-graphql";
+import {Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType, Query } from "type-graphql";
 import argon2 from 'argon2';
 
 @InputType()
@@ -30,10 +30,24 @@ class UserResponse{
 
 @Resolver()
 export class UserResolver{
+
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() {req,em}: MyContext
+    ){
+        // you are not logged in
+        if (!req.session.userId){
+            return null
+        }
+
+        const user = await em.findOne(User, {id: req.session.userId});
+        return user;
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput, // Instead of having multiple args, just have an object
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse>{
         if (options.username.length <= 2){
             return {
@@ -70,6 +84,9 @@ export class UserResolver{
                 }
             }
         }
+
+        req.session.userId = user.id; // Store user id session, this will set a cookie on the user and keep them logged in
+
         return {user};
     }
 
@@ -77,7 +94,7 @@ export class UserResolver{
     @Mutation(() => UserResponse)
     async login(
         @Arg('options') options: UsernamePasswordInput, // Instead of having multiple args, just have an object
-        @Ctx() {em}: MyContext
+        @Ctx() {em, req}: MyContext
     ): Promise<UserResponse>{
         const user = await em.findOne(User, {username: options.username});
         if (!user){
@@ -97,6 +114,9 @@ export class UserResolver{
                 }]
             }
         }
+
+        req.session.userId = user.id;
+
         return {user}
     }
 }
