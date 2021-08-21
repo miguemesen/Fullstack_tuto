9ff7c57bc51da3eeb840d30a5a65e53cff8c8966
miguemesen <miguemesen12@gmail.com>
2021-08-21 13:00:43 -0600

    Minor changes

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -8,7 +8,7 @@ const sleep = (ms: number) => new Promise((res) => setTimeout(res,ms)) // sleep 
 export class PostResolver{
     @Query(() => [Post]) // Queries are for getting data
     async posts(@Ctx() {em}: MyContext): Promise<Post[]>{
-        await sleep(3000);
+        //await sleep(3000);
         return em.find(Post,{});
     }
 
