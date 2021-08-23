865c0dc75ce99c58ca3c5211dc660757744169a0
miguemesen <miguemesen12@gmail.com>
2021-08-23 04:08:10 -0600

    Add create post graphql

--- web/src/pages/create-post.tsx
+++ web/src/pages/create-post.tsx
@@ -3,45 +3,53 @@ import { Formik, Form } from "formik";
 import React from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
+import { useCreatePostMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const CreatePost: React.FC<{}> = ({}) => {
-    return (
-      <Wrapper>
-        <Formik
-          initialValues={{ title: "", text: "" }}
-          onSubmit={async (values) => {
-            console.log(values);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
+  const router = useRouter();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          console.log(values);
+          await createPost({ input: values });
+          router.push("/")
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="title"
+              label="Title"
+              placeholder="title"
+            ></InputField>
+            <Box mt={4}>
               <InputField
-                name="title"
-                label="Title"
-                placeholder="title"
+                textarea
+                name="text"
+                label="Body"
+                placeholder="text..."
               ></InputField>
-              <Box mt={4}>
-                <InputField
-                  textarea
-                  name="text"
-                  label="Body"
-                  placeholder="text..."
-                ></InputField>
-              </Box>
-              <Button
-                mt={4}
-                type="submit"
-                isLoading={isSubmitting}
-                backgroundColor="teal"
-                color="white"
-              >
-                create post
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      </Wrapper>
-    );
-}
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              backgroundColor="teal"
+              color="white"
+            >
+              create post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-export default CreatePost;
+export default withUrqlClient(createUrqlClient) (CreatePost);
