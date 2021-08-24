ce6f0b57f6a5b902a663685e8d1f950b5423db23
miguemesen <miguemesen12@gmail.com>
2021-08-24 04:27:51 -0600

    Minor changes

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,30 +1,45 @@
-import { Box, Link,Stack, Text, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Link,
+  Stack,
+  Text,
+  Heading,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import NextLink from "next/link"
+import NextLink from "next/link";
 import React from "react";
 
-//rerun
 const Index = () => {
-  const [{data}] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
-      limit: 10
-    }
+      limit: 10,
+    },
   });
+
+  if (!fetching && !data) {
+    return <div>you got query failed for some reason</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>create post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map((p) => (
+          {data!.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
@@ -32,8 +47,15 @@ const Index = () => {
           ))}
         </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={8}>
+            load more
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
-}
+};
 
-export default withUrqlClient(createUrqlClient,{ssr:true}) (Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
